c75aa068d681ece9b8de960e828b1e83f9f80b84
Mattèo Gauthier <matteo.gauthier@gmail.com>
2023-02-09 02:48:01 +0100

    let's gooo

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,45 @@
-/**
- * Welcome to Cloudflare Workers! This is your first worker.
- *
- * - Run `wrangler dev src/index.ts` in your terminal to start a development server
- * - Open a browser tab at http://localhost:8787/ to see your worker in action
- * - Run `wrangler publish src/index.ts --name my-worker` to publish your worker
- *
- * Learn more at https://developers.cloudflare.com/workers/
- */
+import { IncomingRequestCfProperties } from "@cloudflare/workers-types"
 
+import { nanoid } from "nanoid"
+
+import { Hono } from "hono"
 export interface Env {
-  // Example binding to KV. Learn more at https://developers.cloudflare.com/workers/runtime-apis/kv/
-  // MY_KV_NAMESPACE: KVNamespace;
-  //
-  // Example binding to Durable Object. Learn more at https://developers.cloudflare.com/workers/runtime-apis/durable-objects/
-  // MY_DURABLE_OBJECT: DurableObjectNamespace;
-  //
-  // Example binding to R2. Learn more at https://developers.cloudflare.com/workers/runtime-apis/r2/
-  // MY_BUCKET: R2Bucket;
+  VIEWS: KVNamespace
 }
 
-export default {
-  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
-    return new Response("Hello World!")
-  },
-}
+const app = new Hono<{ Bindings: Env }>()
+
+app.get("/:projectId/capture", async (c) => {
+  const id = nanoid()
+  const key = `${c.req.param().projectId}:${id}`
+
+  const geo = c.req.cf as any
+
+  let value = await c.env.VIEWS.put(key, `_v${key}`, {
+    metadata: {
+      time: new Date().toISOString(),
+      ip: c.req.headers.get("CF-Connecting-IP") || "unknown",
+      realIp: c.req.headers.get("X-Real-IP") || "unknown",
+      ua: c.req.headers.get("User-Agent") || "unknown",
+      country: geo?.country || "unknown",
+      city: geo?.city || "unknown",
+      region: geo?.region || "unknown",
+      longitude: geo?.longitude || "unknown",
+      latitude: geo?.latitude || "unknown",
+    },
+    expirationTtl: 60 * 30,
+  })
+
+  return c.json({
+    message: "Captured",
+  })
+})
+
+app.get("/:projectId/live-usage", async (c) => {
+  let views = await c.env.VIEWS.list({
+    prefix: `${c.req.param().projectId}:`,
+  })
+  return c.json(views)
+})
+
+export default app
